824ad35155ea9a62cb4c6de02db85e89d4574b3e
Jessica Castro <jessica.castro_storm@prestador.globo>
2021-11-29 21:53:25 -0300

    Inserts final version of the site

--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
-const toggleButton = document.getElementById('toggle-button');
-const navigationList = document.getElementById('navigation-list');
+window.onload = function() {
+  const toggleButton = document.getElementById('toggle-button');
+  const navigationList = document.getElementById('navigation-list');
+  const bodyElement = document.querySelector('body');
 
-toggleButton.addEventListener('click', () => {
-  navigationList.classList.toggle('active');
-  navigationList.classList.toggle('inative');
-});
+  const whiteMode = document.getElementById('whiteMode');
+  const blackMode = document.getElementById('blackMode');
+  const grayMode = document.getElementById('grayMode');
+
+  const fontUp = document.getElementById('fontUp');
+  const fontDown = document.getElementById('fontDown');
+  const increaseDecrease = 0.10;
+  var fontSize = 1;
+
+  toggleButton.addEventListener('click', () => {
+    navigationList.classList.toggle('active');
+    navigationList.classList.toggle('inative');
+  });
+
+  fontUp.addEventListener('click', () => {
+    if (fontSize <= 1.60) {
+      fontSize += increaseDecrease;
+      bodyElement.style.fontSize = fontSize + 'em';
+    }
+  });
+
+  fontDown.addEventListener('click', () => {
+    console.log('clicou diminuir')
+    if (fontSize !== 1) {
+      console.log('entrei')
+      fontSize = fontSize - increaseDecrease;
+      bodyElement.style.fontSize = fontSize + 'em';
+    }
+  });
+
+  whiteMode.addEventListener('click', () => {
+    bodyElement.classList.toggle('whiteMode');
+  })
+
+  blackMode.addEventListener('click', () => {
+    bodyElement.classList.toggle('blackMode');
+  })
+}
